a82e09ac27ef175b6feeab7d940aa8af65ecf91a
xiaoxihahaha <lxx_xiaoxihahaha@163.com>
2021-09-18 16:21:29 +0800

    调试测试

---
+++ src/com/atguigu/java/DebugTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.java;
+
+import java.util.HashMap;
+
+/**
+ * @author linxiaoxi
+ * @date 2021/9/18 - 16:14
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name","Tom");
+        map.put("age","12");
+        map.put("school","Tsinghua");
+        map.put("major","computer");
+
+    }
+}
